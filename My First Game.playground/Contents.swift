c03ebf3358fcc1d0093cffdd36dc88aacc91b63c
Tu Nguyen <nvminhtu@gmail.com>
2017-02-21 12:43:22 +0700

    First  background for IOS

---
+++ My First Game.playground/Contents.swift
@@ -0,0 +1,43 @@
+//playground: where a people can try to code and play
+
+import UIKit
+
+var str = "hello"
+var customStr:String = "My message"
+
+var number = 10 + 8 + 9 + 10
+
+//ghi chu:
+var number2 = number + 4 + 4
+
+print(number2)
+
+// Many basic data types: Boolean, Int, Foat, Double
+var myDoubleNumber = 1.0
+
+// Xcode will regconize type of data ( example: float will be store as Double Type)
+myDoubleNumber
+
+var myFloatNumber = 10.7
+
+// setup variables for checking data
+var isAnimal:Bool = true
+var isMember:Bool = false
+
+print(isAnimal)
+
+// phan biet let & var
+// let: swift se hieu gia tri dinh cho no la 1 hang so
+let myName:String = "welcome to my team"
+//myName = "Viet"
+
+// special assign
+let 😬 = "smiley"
+
+print(😬)
+
+let myFace = "😘"
+print(myFace)
+
+
+
