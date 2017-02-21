2bc54d9350602a13cf8558b800b4162a1647b74c
Tu Nguyen <nvminhtu@gmail.com>
2017-02-21 13:49:40 +0700

    optional swift

---
+++ condition.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var a = 30
+var b = 20
+
+if b > a {
+    b - a
+} else {
+    a - b
+}
+
+//chuoi
+var chuoi1 = "IDE"
+var chuoi2 = "ID"
+
+if chuoi1 != chuoi2 {
+    print("aaaa")
+} else {
+    print("no khac roi")
+}
+
+var x:Bool = true
+var y:Bool = false
+
+if x == false {
+    print("no ban false roi")
+}
+if !x {
+    print("bien no bang false roi")
+}
+if x == true && y == false {
+    print("we are here")
+}
---
+++ optional.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// nil
+var bien:Int? = 10
+
+var bien_null:Int? = 8
+var bien2:Int! = 8
+
+print(bien)
+bien2.description
+
+print(bien_null!) // bo chu optional
+print(bien_null) // show ca chu optional()
+
+// unwrap() vs wrap(?)
+// wrap(?)
+//var bienSo:Int! = Int("aafaf")!
+
+// If let
