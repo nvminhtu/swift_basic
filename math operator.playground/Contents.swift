ac849945abde9d262ac0403a3e09cd76833a199f
Tu Nguyen <nvminhtu@gmail.com>
2017-02-21 12:56:00 +0700

    finish testing for math operator

--- math operator.playground/Contents.swift
+++ math operator.playground/Contents.swift
@@ -3,3 +3,36 @@
 import UIKit
 
 var str = "Hello, playground"
+
+10 + 7
+
+var so1:Int = 20
+var so2:Int = 30
+
+so1 + so2
+so2 - so1
+so1 * so2
+so2 / 5
+so1 % 11
+
+var chuoi1:String = "Toi ten la"
+var chuoi2:String = "Tu"
+
+chuoi1 + " " + chuoi2
+
+// setup so 
+var soNguyen:Int = 5
+var soThuc:Float = 10
+
+
+// Ep kieu
+Float(soNguyen) + soThuc
+
+var so:Int = 10
+var chuoi:String = "50"
+
+// dau ? va ! trong Swift ???
+so + Int(chuoi)!
+
+String(so) + chuoi
+String(so) + " va " + chuoi
